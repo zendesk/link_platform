e2942d6e4e1092c17733f3f22cb95b044335555f
Adam Hart <ahart@zendesk.com>
2018-07-25 17:02:22 -0700

    Alignment

--- link-api/app/models/program.rb
+++ link-api/app/models/program.rb
@@ -2,5 +2,5 @@ class Program < ApplicationRecord
   belongs_to :organization
 
   validates :organization_id, presence: true
-  validates :name,            presence: true
+  validates :name, presence: true
 end
