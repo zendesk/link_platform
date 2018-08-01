a59a0e0b2cf6788533d330179ee9e84480073947
Craig Day <cday@zendesk.com>
2018-08-01 14:57:58 -0700

    connect the new models to each other

--- link-api/spec/factories/contacts.rb
+++ link-api/spec/factories/contacts.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :contact do
     organization
-    # service # TODO: model doesn't exist yet
-    # service_at_location # TODO: model doesn't exist yet
-    service_id "123" # TODO: remove this after the service factory exists
-    service_at_location_id "123" # TODO: remove this after service_at_location factory exists
+    service
+    service_at_location
     name "Jennifer Hanson"
     title "Contact Person"
     department "Help Department"
