736d2b1e1c2cb31ee743ecc16019a666b251c664
Adam Hart <ahart@zendesk.com>
2018-08-08 15:03:20 -0700

    Add Admin Name and re-migrate

--- link-api/db/migrate/20180801230220_devise_create_admins.rb
+++ link-api/db/migrate/20180801230220_devise_create_admins.rb
@@ -3,6 +3,8 @@
 class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
   def change
     create_table :admins do |t|
+      t.string :name, null: false, default: ""
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -32,13 +34,12 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
       t.string   :unlock_token # Only if unlock strategy is :email or :both
       t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :admins, :email,                unique: true
+    add_index :admins, :email
     add_index :admins, :reset_password_token, unique: true
-    # add_index :admins, :confirmation_token,   unique: true
-    # add_index :admins, :unlock_token,         unique: true
+    add_index :admins, :confirmation_token,   unique: true
+    add_index :admins, :unlock_token,         unique: true
   end
 end
--- link-api/db/schema.rb
+++ link-api/db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2018_08_01_230220) do
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -35,8 +36,10 @@ ActiveRecord::Schema.define(version: 2018_08_01_230220) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_admins_on_email"
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
   end
 
   create_table "contacts", force: :cascade do |t|
