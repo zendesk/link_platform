bad3a55f2b90a1a2593ee484651300ebc0c91378
Rose Trujillo <roseleaf.red@gmail.com>
2018-10-31 16:17:10 -0700

    Fix test

---
+++ link-api/db/migrate/20181031235244_add_link_instance_to_organizations.rb
@@ -0,0 +1,5 @@
+class AddLinkInstanceToOrganizations < ActiveRecord::Migration[5.2]
+  def change
+    add_column :organizations, :link_instance_id, :string, null: false
+  end
+end
--- link-api/spec/routing/organizations_routing_spec.rb
+++ link-api/spec/routing/organizations_routing_spec.rb
@@ -4,29 +4,29 @@ RSpec.describe Api::OrganizationsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/api/organizations").to route_to("/api/organizations#index")
+      expect(:get => "/api/organizations").to route_to("api/organizations#index")
     end
 
 
     it "routes to #show" do
-      expect(:get => "/api/organizations/1").to route_to("/api/organizations#show", :id => "1")
+      expect(:get => "/api/organizations/1").to route_to("api/organizations#show", :id => "1")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/api/organizations").to route_to("/api/organizations#create")
+      expect(:post => "/api/organizations").to route_to("api/organizations#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/api/organizations/1").to route_to("/api/organizations#update", :id => "1")
+      expect(:put => "/api/organizations/1").to route_to("api/organizations#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/api/organizations/1").to route_to("/api/organizations#update", :id => "1")
+      expect(:patch => "/api/organizations/1").to route_to("api/organizations#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/api/organizations/1").to route_to("/api/organizations#destroy", :id => "1")
+      expect(:delete => "/api/organizations/1").to route_to("api/organizations#destroy", :id => "1")
     end
 
   end
