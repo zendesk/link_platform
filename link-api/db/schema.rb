ce4f38e36864f0824813b01e4a147ef591ff2ef5
Adam Hart <ahart@zendesk.com>
2019-07-17 13:08:04 -0700

    Rebase commit

--- link-api/db/schema.rb
+++ link-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_201155) do
+ActiveRecord::Schema.define(version: 2019_07_17_000826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -271,6 +271,15 @@ ActiveRecord::Schema.define(version: 2019_06_19_201155) do
     t.index ["program_id"], name: "index_services_on_program_id"
   end
 
+  create_table "sessions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
   create_table "taxonomies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "link_instance_id"
     t.string "name", null: false
@@ -280,15 +289,6 @@ ActiveRecord::Schema.define(version: 2019_06_19_201155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["link_instance_id"], name: "index_taxonomies_on_link_instance_id"
-  end
-
-  create_table "sessions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "session_id", null: false
-    t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   add_foreign_key "contacts", "link_instances"
