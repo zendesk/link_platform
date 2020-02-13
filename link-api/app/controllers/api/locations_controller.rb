17a4eca088e4a74a3bde935fc30e31841505190c
Carl Goldberg <carlgoldberg1236@gmail.com>
2020-02-12 16:06:06 -0800

    Module-ize controllers

--- link-api/app/controllers/api/locations_controller.rb
+++ link-api/app/controllers/api/locations_controller.rb
@@ -1,57 +1,58 @@
 # frozen_string_literal: true
+module Api
+  class LocationsController < ApiBaseController
+    before_action :set_location, only: %i[show update destroy]
 
-class Api::LocationsController < ApiBaseController
-  before_action :set_location, only: %i[show update destroy]
+    # GET /locations
+    def index
+      @locations = current_link_instance.locations.all
 
-  # GET /locations
-  def index
-    @locations = current_link_instance.locations.all
-
-    render json: @locations
-  end
-
-  # GET /locations/1
-  def show
-    render json: @location
-  end
-
-  # POST /locations
-  def create
-    @location = Location.new(location_params)
-    @location.link_instance = current_link_instance
-
-    if @location.save
-      render json: @location,
-             status: :created,
-             location: api_location_url(@location)
-    else
-      render json: @location.errors, status: :unprocessable_entity
+      render json: @locations
     end
-  end
 
-  # PATCH/PUT /locations/1
-  def update
-    if @location.update(location_params)
+    # GET /locations/1
+    def show
       render json: @location
-    else
-      render json: @location.errors, status: :unprocessable_entity
     end
-  end
 
-  # DELETE /locations/1
-  def destroy
-    @location.destroy
-  end
+    # POST /locations
+    def create
+      @location = Location.new(location_params)
+      @location.link_instance = current_link_instance
 
-  private
+      if @location.save
+        render json: @location,
+               status: :created,
+               location: api_location_url(@location)
+      else
+        render json: @location.errors, status: :unprocessable_entity
+      end
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_location
-    @location = current_link_instance.locations.find(params[:id])
-  end
+    # PATCH/PUT /locations/1
+    def update
+      if @location.update(location_params)
+        render json: @location
+      else
+        render json: @location.errors, status: :unprocessable_entity
+      end
+    end
 
-  # Only allow a trusted parameter "white list" through.
-  def location_params
-    params.require(:location).permit(LOCATION_PARAMS)
+    # DELETE /locations/1
+    def destroy
+      @location.destroy
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_location
+      @location = current_link_instance.locations.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def location_params
+      params.require(:location).permit(LOCATION_PARAMS)
+    end
   end
 end
--- link-api/app/controllers/api/programs_controller.rb
+++ link-api/app/controllers/api/programs_controller.rb
@@ -1,56 +1,57 @@
 # frozen_string_literal: true
+module Api
+  class ProgramsController < ApiBaseController
+    before_action :set_program, only: %i[show update destroy]
 
-class Api::ProgramsController < ApiBaseController
-  before_action :set_program, only: %i[show update destroy]
+    # GET /programs
+    def index
+      @programs = current_link_instance.programs.all
 
-  # GET /programs
-  def index
-    @programs = current_link_instance.programs.all
-
-    render json: @programs
-  end
-
-  # GET /programs/1
-  def show
-    render json: @program
-  end
-
-  # POST /programs
-  def create
-    @program = current_link_instance.programs.build(program_params)
-
-    if @program.save
-      render json: @program,
-             status: :created,
-             location: api_program_url(@program)
-    else
-      render json: @program.errors, status: :unprocessable_entity
+      render json: @programs
     end
-  end
 
-  # PATCH/PUT /programs/1
-  def update
-    if @program.update(program_params)
+    # GET /programs/1
+    def show
       render json: @program
-    else
-      render json: @program.errors, status: :unprocessable_entity
     end
-  end
 
-  # DELETE /programs/1
-  def destroy
-    @program.destroy
-  end
+    # POST /programs
+    def create
+      @program = current_link_instance.programs.build(program_params)
 
-  private
+      if @program.save
+        render json: @program,
+               status: :created,
+               location: api_program_url(@program)
+      else
+        render json: @program.errors, status: :unprocessable_entity
+      end
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_program
-    @program = current_link_instance.programs.find(params[:id])
-  end
+    # PATCH/PUT /programs/1
+    def update
+      if @program.update(program_params)
+        render json: @program
+      else
+        render json: @program.errors, status: :unprocessable_entity
+      end
+    end
 
-  # Only allow a trusted parameter "white list" through.
-  def program_params
-    params.require(:program).permit(PROGRAM_PARAMS)
+    # DELETE /programs/1
+    def destroy
+      @program.destroy
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_program
+      @program = current_link_instance.programs.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def program_params
+      params.require(:program).permit(PROGRAM_PARAMS)
+    end
   end
 end
