79dd9911ebab981281cbad07401c430b2d9b075d
Avram Walden <aviemet@gmail.com>
2020-09-03 19:09:26 -0700

    Updates zendeskgarden

--- link-admin/src/pages/Landing/components/OrganizationList.js
+++ link-admin/src/pages/Landing/components/OrganizationList.js
@@ -2,51 +2,60 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { createComponent } from 'react-fela'
 import partial from 'lodash/partial'
-import Tables from '@zendeskgarden/react-tables'
+import {
+  Table,
+  Caption,
+  Head,
+  HeaderRow,
+  HeaderCell,
+  Body,
+  Row,
+  Cell
+} from '@zendeskgarden/react-tables'
 import OverflowMenu from './OverflowMenu'
 
 const OrganizationList = ({ organizations, onSelectEdit, onSelectDelete }) => (
-  <Tables.Table>
+  <Table>
 
-    <Tables.Head>
-      <Tables.HeaderRow>
-        <Tables.HeaderCell width="50%">Name</Tables.HeaderCell>
-        <Tables.HeaderCell width="25%">URL</Tables.HeaderCell>
-        <Tables.HeaderCell width="25%">Updated</Tables.HeaderCell>
-        <Tables.HeaderCell menu />
-      </Tables.HeaderRow>
-    </Tables.Head>
+    <Head>
+      <HeaderRow>
+        <HeaderCell width="50%">Name</HeaderCell>
+        <HeaderCell width="25%">URL</HeaderCell>
+        <HeaderCell width="25%">Updated</HeaderCell>
+        <HeaderCell hasOverflow />
+      </HeaderRow>
+    </Head>
     
-    <Tables.Body>
+    <Body>
       {organizations.map(organization => (
-        <Row
+        <OrgRow
           key={ organization.id }
           organization={ organization }
           onEdit={ onSelectEdit }
           onDelete={ onSelectDelete }
         />
       ))}
-    </Tables.Body>
+    </Body>
 
-  </Tables.Table>
+  </Table>
 )
 
-const Row = ({ organization, onEdit, onDelete }) => {
+const OrgRow = ({ organization, onEdit, onDelete }) => {
   const onEditOrganization = partial(onEdit, organization)
   const onDeleteOrganization = partial(onDelete, organization)
 
   return (
-    <Tables.Row>
-      <Tables.Cell width="50%" onClick={ onEditOrganization }>
+    <Row>
+      <Cell width="50%" onClick={ onEditOrganization }>
         { organization.name }
-      </Tables.Cell>
-      <Tables.Cell width="25%" onClick={ onEditOrganization }>
+      </Cell>
+      <Cell width="25%" onClick={ onEditOrganization }>
         { organization.url }
-      </Tables.Cell>
-      <Tables.Cell width="25%" onClick={ onEditOrganization }>
+      </Cell>
+      <Cell width="25%" onClick={ onEditOrganization }>
         { organization.updatedAt }
-      </Tables.Cell>
-      <Tables.Cell menu>
+      </Cell>
+      <Cell>
         <OverflowMenuPadding>
           <OverflowMenu
             onSelectItem={ partial(
@@ -57,8 +66,8 @@ const Row = ({ organization, onEdit, onDelete }) => {
             menuItems={ overflowItems() }
           />
         </OverflowMenuPadding>
-      </Tables.Cell>
-    </Tables.Row>
+      </Cell>
+    </Row>
   )
 }
 
