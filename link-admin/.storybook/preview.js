bc82b093c8c2d11ac2d3450b8001cf210d08f1b4
Avram Walden <aviemet@gmail.com>
2020-12-05 14:20:24 -0800

    Updates storybook config for styled components

--- link-admin/.storybook/preview.js
+++ link-admin/.storybook/preview.js
@@ -1,21 +1,16 @@
 import React from 'react'
-import { RendererProvider as FelaProvider } from 'react-fela'
+// import { RendererProvider as FelaProvider } from 'react-fela'
+import { ThemeProvider as StyledThemeProvider } from 'styled-components'
+import styledTheme from '../src/app/styledTheme'
 import { configure, addDecorator } from '@storybook/react'
 
-import { Provider } from 'react-redux'
+import { Provider as ReduxProvider } from 'react-redux'
 import store from 'store'
-import { createRenderer } from 'fela'
 
-import '../src/index.css'
-
-const renderer = createRenderer()
-
-const withFelaProvider = story => (
-  <FelaProvider renderer={renderer}>{ story() }</FelaProvider>
-)
+import '../src/app/index.css'
 
 const withReduxProvider = story => (
-  <Provider store={ store }>{ story() }</Provider>
+  <ReduxProvider store={ store }>{ story() }</ReduxProvider>
 )
 
 const req = require.context('../src', true, /\.stories.js$/)
@@ -24,6 +19,15 @@ function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
 
-addDecorator(withFelaProvider)
 addDecorator(withReduxProvider)
 configure(loadStories, module)
+
+export const decorators = [
+  (Story) => (
+    <ReduxProvider>
+      <StyledThemeProvider theme={ styledTheme }>
+        <Story />
+      </StyledThemeProvider>
+    </ReduxProvider>
+  )
+]
