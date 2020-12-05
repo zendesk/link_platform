5e30db89b156dde768e3cd24d6bf37f28be48bc7
Avram Walden <aviemet@gmail.com>
2020-12-05 14:02:04 -0800

    Replaces Fela component tests with styled-components in test file

--- link-admin/src/pages/Landing/Taxonomy/Taxonomy.test.js
+++ link-admin/src/pages/Landing/Taxonomy/Taxonomy.test.js
@@ -1,46 +1,47 @@
 import React from 'react'
+import { mount } from 'enzyme'
+import 'jest-styled-components'
 import * as Taxonomy from '.'
-import { shallow } from 'utils/testing/fela'
+import { Tag as GardenTag } from '@zendeskgarden/react-tags'
+import Spacer from 'components/layout/Spacer'
 
 const mockOnClick = jest.fn()
-const setShallowTagWrapper = (props = {}) => shallow(
-  <Taxonomy.Tag
-    onClick={ mockOnClick }
-    isActive={ false }
-    taxonomy={{ id: 1, index: 0, label: 'foobar' }}
-    { ...props }
-  />
-)
+
+const Tag = (props = {}) => <Taxonomy.Tag
+  onClick={ mockOnClick }
+  isActive={ false }
+  taxonomy={{ id: 1, index: 0, label: 'foobar' }}
+  { ...props }
+/>
 
 describe('<Tag />', () => {
   it('renders correct taxonomy label', () => {
-    const wrapper = setShallowTagWrapper()
-    expect(wrapper.find('Tag').render().text()).toEqual('foobar')
+    const wrapper = mount(<Tag />)
+
+    expect(wrapper.find(GardenTag).text()).toEqual('foobar')
   })
 
   it('renders as grey if inactive', () => {
-    const wrapper = setShallowTagWrapper()
+    const wrapper = mount(<Tag />)
 
-    expect(wrapper.find('Tag').prop('type')).toEqual('grey')
+    expect(wrapper.find(GardenTag).prop('type')).toEqual('grey')
   })
 
   it('renders as blue if active', () => {
-    const wrapper = setShallowTagWrapper({ isActive: true })
+    const wrapper = mount(<Tag isActive={ true } />)
 
-    expect(wrapper.find('Tag').prop('type')).toEqual('blue')
+    expect(wrapper.find(GardenTag).prop('type')).toEqual('blue')
   })
 
   it('does not add a spacer if it is the first tag', () => {
-    const wrapper = setShallowTagWrapper()
+    const wrapper = mount(<Tag />)
 
-    expect(wrapper.find('FelaComponent[space=".5"]')).toHaveLength(0)
+    expect(wrapper.find(Spacer)).toHaveLength(0)
   })
 
   it('adds a spacer if not the first tag', () => {
-    const wrapper = setShallowTagWrapper({
-      taxonomy: { id: 1, index: 1, label: 'foobar' }
-    })
+    const wrapper = mount(<Tag taxonomy={{ id: 1, index: 1, label: 'foobar' }} />)
 
-    expect(wrapper.find('FelaComponent[space=".5"]')).toHaveLength(1)
+    expect(wrapper.find(Spacer)).toHaveLength(1)
   })
 })
