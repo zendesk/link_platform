bd622090584f01f671ddc0bcff0b4ff93c5496c8
Avram Walden <aviemet@gmail.com>
2021-01-02 18:23:12 -0800

    Makes search bar clearable

--- link-admin/src/pages/Landing/AdminTopBar/index.jsx
+++ link-admin/src/pages/Landing/AdminTopBar/index.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
 import { Button } from '@zendeskgarden/react-buttons'
-import { Field, Input } from '@zendeskgarden/react-forms'
 import { useNavigation } from 'components/Routing'
+
+import { Field, MediaInput } from '@zendeskgarden/react-forms'
+import { ReactComponent as SearchIcon } from '@zendeskgarden/svg-icons/src/16/search-stroke.svg'
+import { ReactComponent as XIcon } from '@zendeskgarden/svg-icons/src/16/x-stroke.svg'
 
 import strings from 'strings'
 import Row from 'components/layout/Row'
 import TaxonomyTags from '../Taxonomy'
 
 const AdminTopBar = () => {
+  const [orgsFilter, setOrgsFilter] = useState('')
   const navigate = useNavigation()
 
+  const handleSearchInput = e => {
+    setOrgsFilter(e.target.value)
+  }
+  
   return (
     <div>
       <Row className="flex flex-wrap w-full gap-2">
         <Field className="flex-grow">
-          <Input
+          <MediaInput
+            onChange={ handleSearchInput }
+            end={ <SearchInputIcon value={ orgsFilter } onClick={ () => setOrgsFilter('') } /> }
+            value={ orgsFilter }
             placeholder={ strings.AdminTopBar_SearchPlaceholder }
             aria-label={ strings.AdminTopBar_SearchPlaceholder }
           />
@@ -29,6 +42,18 @@ const AdminTopBar = () => {
       <TaxonomyTags />
     </div>
   )
+}
+
+const SearchInputIcon = ({ value, onClick }) => {
+  if(value === '') {
+    return <SearchIcon />
+  }
+  return <XIcon onClick={ onClick } className="cursor-pointer" />
+}
+
+SearchInputIcon.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export default AdminTopBar
